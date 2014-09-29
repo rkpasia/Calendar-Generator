2584cd35e350414ea17fa57b109941ddfa9767ce
rkpasia <riccardo.pasianotto@gmail.com>
2014-09-29 11:44:49 +0200

    Add jquery and $.get request

--- calendar-generator.js
+++ calendar-generator.js
@@ -1,13 +1,15 @@
-var url = "https://orari.uniud.it/EasyCourse/Orario/Polo_Scientifico/2014-2015/327/Curricula/Tecnologiewebemultimediali_LaureatriennaleDM270_2_Corsogenerico_726.html";
+var htmlPage = "";
 
-httpGet();
+$.get(
+	"http://orari.uniud.it/EasyCourse/Orario/Polo_Scientifico/2014-2015/327/Curricula/Tecnologiewebemultimediali_LaureatriennaleDM270_2_Corsogenerico_726.html",
+	function(data){
+		console.log("Load performed");
+		//console.log(data);
+		htmlPage = data;
+		fetchData(htmlPage);
+	}
+);
 
-function httpGet(url){
-    var xmlHttp = null;
-
-    xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", url, false );
-    xmlHttp.send( null );
-    console.log(xmlHttp.responseText);
-    return xmlHttp.responseText;
+function fetchData(htmlPage){
+	console.log(htmlPage);
 }
