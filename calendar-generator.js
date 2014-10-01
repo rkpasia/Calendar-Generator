1e2ff82851e2243a3b948bb3b26be35ea4df4762
rkpasia <riccardo.pasianotto@gmail.com>
2014-10-01 15:31:14 +0200

    Google access code

--- calendar-generator.js
+++ calendar-generator.js
@@ -33,78 +33,16 @@ function fetchData(htmlPage){
 	console.log(contentTable);
 }
 
-var OAUTHURL    =   'https://accounts.google.com/o/oauth2/auth?';
-var VALIDURL    =   'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=';
-var SCOPE       =   'https://www.googleapis.com/auth/userinfo.profile';
-var CLIENTID    =   '221078581487-oph0qts3kh9bfppciv3u3r9h87liugdo.apps.googleusercontent.com';
-var REDIRECT    =   'http://rkpasia.github.io/oauth2callback'
-var TYPE        =   'token';
-var _url        =   OAUTHURL + 'scope=' + SCOPE + '&client_id=' + CLIENTID + '&redirect_uri=' + REDIRECT + '&response_type=' + TYPE;
-
-
-function login() {
-  var win         =   window.open(_url, "windowname1", 'width=800, height=600'); 
-
-  var pollTimer   =   window.setInterval(function() { 
-      try {
-          console.log(win.document.URL);
-          if (win.document.URL.indexOf(REDIRECT) != -1) {
-              window.clearInterval(pollTimer);
-              var url =   win.document.URL;
-              acToken =   gup(url, 'access_token');
-              tokenType = gup(url, 'token_type');
-              expiresIn = gup(url, 'expires_in');
-              win.close();
-
-              validateToken(acToken);
-          }
-      } catch(e) {
-      }
-  }, 100);
+function signinCallback(authResult) {
+  if (authResult['access_token']) {
+    // Autorizzazione riuscita
+    // Nascondi il pulsante di accesso ora che l'utente è autorizzato. Ad esempio: 
+    document.getElementById('signinButton').setAttribute('style', 'display: none');
+  } else if (authResult['error']) {
+    // Si è verificato un errore.
+    // Possibili codici di errore:
+    //   "access_denied" - L'utente ha negato l'accesso alla tua app
+    //   "immediate_failed" - Impossibile eseguire l'accesso automatico dell'utente
+    // console.log('There was an error: ' + authResult['error']);
+  }
 }
-
-function validateToken(token) {
-  $.ajax({
-    url: VALIDURL + token,
-    data: null,
-    success: function(responseText){  
-        getUserInfo();
-    },  
-    dataType: "jsonp"  
-  });
-}
-
-//credits: http://www.netlobo.com/url_query_string_javascript.html
-function gup(url, name) {
-  name = name.replace(/[[]/,"\[").replace(/[]]/,"\]");
-  var regexS = "[\?&]"+name+"=([^&#]*)";
-  var regex = new RegExp( regexS );
-  var results = regex.exec( url );
-  if( results == null )
-      return "";
-  else
-      return results[1];
-}
-
-function getUserInfo() {
-    $.ajax({
-        url: 'https://www.googleapis.com/oauth2/v1/userinfo?access_token=' + acToken,
-        data: null,
-        success: function(resp) {
-            user    =   resp;
-            console.log(user);
-            $('#uName').append(user.name);
-            $('#imgHolder').attr('src', user.picture);
-        },
-        dataType: "jsonp"
-    });
-}
-
-function startLogoutPolling() {
-  $('#loginText').show();
-  $('#logoutText').hide();
-  loggedIn = false;
-  $('#uName').text('Welcome ');
-  $('#imgHolder').attr('src', 'none.jpg');
-}
-
