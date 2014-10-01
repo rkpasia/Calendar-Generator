1f5be87711876e5dd232e5f0c5809020586b26c7
rkpasia <riccardo.pasianotto@gmail.com>
2014-10-01 14:26:22 +0200

    Uploading data

--- calendar-generator.js
+++ calendar-generator.js
@@ -1,8 +1,8 @@
-var htmlPageUrl = "https://orari.uniud.it/EasyCourse/Orario/Polo_Scientifico/2014-2015/327/Curricula/Informatica_LaureatriennaleDM270_2_Corsogenerico_725.html";
+var htmlPageUrl = "Curricula/Tecnologiewebemultimediali_LaureatriennaleDM270_2_Corsogenerico_726.html";
 
 var fetchedTable = {};
 
-/*$.get(
+$.get(
 	htmlPageUrl,
 	function(data){
 		console.log("//////////////////////////////////////////Load performed");
@@ -11,58 +11,7 @@ var fetchedTable = {};
 
 		fetchData(htmlPage);
 	}
-);*/
-
-
-function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
-    // XHR for Chrome/Firefox/Opera/Safari.
-    xhr.open(method, url, true);
-  } else if (typeof XDomainRequest != "undefined") {
-    // XDomainRequest for IE.
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-  } else {
-    // CORS not supported.
-    xhr = null;
-  }
-  return xhr;
-}
-
-// Helper method to parse the title tag from the response.
-function getTitle(text) {
-  return text.match('<title>(.*)?</title>')[1];
-}
-
-// Make the actual CORS request.
-function makeCorsRequest() {
-  // All HTML5 Rocks properties support CORS.
-  
-
-  var xhr = createCORSRequest('GET', htmlPageUrl);
-  if (!xhr) {
-    alert('CORS not supported');
-    return;
-  }
-
-  // Response handlers.
-  xhr.onload = function() {
-    var text = xhr.responseText;
-    var title = getTitle(text);
-    alert('Response from CORS request to ' + htmlPageUrl + ': ' + title);
-  };
-
-  xhr.onerror = function() {
-    alert('Woops, there was an error making the request.');
-  };
-
-  xhr.send();
-  console.log('Ho fatto qualcosa');
-}
-
-makeCorsRequest();
-
+);
 
 function fetchData(htmlPage){
 	var timeTable = $(htmlPage).find('table.timegrid');
